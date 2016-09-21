b5842289d3a61f7adb503055bae86303e2791706
alexandermendes <alexanderhmendes@gmail.com>
2016-09-21 17:14:37 +0100

    Fix tests

--- test/test_main.py
+++ test/test_main.py
@@ -15,12 +15,13 @@ class TestConverter():
         mocker.patch('marc2excel.main.Converter.excel2marc')
         marc2excel.excel2marc(tmp_xlsx, tmp_mrc)
         Converter.excel2marc.assert_called_once_with(tmp_xlsx, tmp_mrc,
-                                                     sheet=0)
+                                                     sheet=0, force_utf8=False)
 
     def test_marc2excel_convenience_method(self, tmp_mrc, tmp_xlsx, mocker):
         mocker.patch('marc2excel.main.Converter.marc2excel')
         marc2excel.marc2excel(tmp_mrc, tmp_xlsx)
-        Converter.marc2excel.assert_called_once_with(tmp_mrc, tmp_xlsx)
+        Converter.marc2excel.assert_called_once_with(tmp_mrc, tmp_xlsx,
+                                                     force_utf8=False)
 
     def test_marc2excel_converts_all_records(self, marc_path, tmp_xlsx):
         converter.marc2excel(marc_path, tmp_xlsx)
